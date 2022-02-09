6da58d858532ded08e524a37cc8dbc97943b3fef
Trigun Joshi <trigunjoshi@gmail.com>
2022-02-08 18:03:45 -0800

    spec/features/product_details_spec.rb completed

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to the product page", type: :feature, js: true do
+
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    #CLICK
+    # click_on ".btn-default"
+    first(".btn.btn-default").click()
+    sleep 5
+    visit '/products/1'
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_css '.product-detail'
+  end
+
+end
